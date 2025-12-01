d4df3570d48ba29aa05b83b5862104b9d5c14311
Alabama System Control One <alabama.system@gmail.com>
2025-12-01 11:55:47 -0300

    feat: production

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
 {
-outputs = { self, nixpkgs, flake-utils }:
+
+  description = "Pure GNU Determinate Nix Go Workbench"; # Add description if missing
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils }:
     let 
-      # Define systems to support for evaluation
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
       
-      # Helper function to get pkgs for a system
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system (import nixpkgs { inherit system; }));
       
-      # Use a dummy packages set to get access to pkgs for a target system
-      # We'll use this to define the lib.buildGo function generically.
       pkgs_x86_64 = import nixpkgs { system = "x86_64-linux"; };
 
     in {
-      # 1. lib: DEFINED AT THE TOP LEVEL (This is the critical fix)
-      # We define a single, system-agnostic 'lib' that contains the builder
       lib = {
         buildGo = args: pkgs_x86_64.buildGoModule (args // { });
       };
 
-      # 2. devShells: DEFINED VIA ITERATION (This is for the workbench)
       devShells = forAllSystems (system: pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [ 
@@ -30,8 +31,16 @@ outputs = { self, nixpkgs, flake-utils }:
           '';
         };
       });
-      
-      # NOTE: Remove the flake-utils input if you adopt this structure completely
-      # OR, use flake-utils to define the devShells and keep the top-level lib.
+
+      packages = forAllSystems (system: pkgs: {
+        open-andes-go = pkgs.stdenv.mkDerivation {
+          pname = "open-andes-go";
+          version = "0.0.1";
+          src = pkgs.lib.cleanSource ./.; 
+	  installPhase = "mkdir -p $out";
+        };
+        
+        default = self.packages.${system}.open-andes-go;
+      });
     };
 }
